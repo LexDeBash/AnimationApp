fe9dfd4067f176a396ec255850fcc1cea1b9bbc1
Alexey Efimov <lex.efimov@gmail.com>
2020-03-02 13:46:36 +0500

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Alexey Efimov on 02.03.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
